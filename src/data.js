fc666274fe85d7c545a450cface2239e5e36b568
edmondhabimana <edmond.habimana@yahoo.com>
2024-01-09 19:17:38 -0700

    create data file

---
+++ src/data.js
@@ -0,0 +1,61 @@
+import pictures from "./assets/index";
+const data = [
+  {
+    products: [pictures[0], pictures[1], pictures[2], pictures[3]],
+    name: 'boAt Party Pal 50',
+    details: "Immerse in the 20W RMS Stereo Sound with the powerful Party Pal 50 bluetooth speaker. Set the vibes of the party just right with the RGB LEDs. Keep worries at bay and party poolside with IPX5 splash resistant! Get, set, grooving as Party Pal 50 comes with a powerful playback of 4.5 Hrs. Access Instant Voice Assistant and enjoy the multiple connectivity modes- USB, AUX, Bluetooth v5.1, and FM. Coming with Type-C interface, Party Pal 50 is all you need to set the mood just about right. So, where y’all partying at?",
+    price: 56,
+    quantity: 1
+  },
+  {
+    products: [pictures[4], pictures[5], pictures[6], pictures[7]],
+    name: 'boAt Rockerz 333',
+    details: "When your taste in music is so unique, why should your audio wear be any different? Speak your unique style with Rockerz 333 - a bold and powerful wireless earphones for the part of you that wants more. More of sound, more of style, and more of power. Immerse into your jam with boAt Signature Sound and 30 HRS of non stop playback. Let nothing limit your hustle with Dual Pairing that lets you connect two devices at once. Don’t hold back, choose your vibe with Rockerz 333.",
+    price: 55,
+    quantity: 1
+  },
+  {
+    products: [pictures[8], pictures[9], pictures[10], pictures[11]],
+    name: 'Watch Blaze',
+    details: "Whatever you need to hustle to your goal — now do it Faster with Blaze. It’s powered by Apollo 3 Blue Plus high-performance processor that makes its interface 25% faster. Navigate through your world effortlessly with its 1.75” HD display — the largest by boAt. We understand your hustle, that’s why its Fast Charge technology gives 24HRS of backup with just 10MINS of charge. Keep your fitness in check with heart rate and SpO2 monitoring, daily activity tracker, and 14 sport modes. Smart Watch Blaze — for trailblazers like you.",
+    price: 49,
+    quantity: 1
+  },
+  {
+    products: [pictures[12], pictures[13], pictures[14], pictures[15]],
+    name: 'boAt BassHeads 242',
+    details: "Get ready to redefine fitness fashion and utility with the wired headphone boAt Bassheads 242, it’s time to get your sport on. It carries ear hooks that are crafted from silicon to maximize comfort and a secure fit that allows you to maximize your workouts. Coated with IPX 4 sweat and water protection, the Bassheads 242 emphasizes on all-around ability with 10mm drivers that reproduce a quality sound. Move around freely with the superior coated cable that reaches comfortably from your pocket till your ears. Connect to a number of devices with ease using the 3.5mm straight jack, because there’s no stress, only sweetness when you’re riding the waves on this boAt. You can tune in and out with the pause/play button that can also take calls (With In-line Mic.) and control volume in-between exercise reputations.",
+    price: 34,
+    quantity: 1
+  },
+  {
+    products: [pictures[16], pictures[17], pictures[18], pictures[19]],
+    name: 'boAt Immortal 1000D',
+    details: "The game begins here. With Immortal 1000D gaming headphones, don’t just play the game - feel it, live it, and own it. Level up your audio game with 7.1 Channel Surround Sound and Dolby Atmos that unleashes the winning beast inside you. Equipped with 50mm drivers, get a powerful 360° gaming experience like no other. Now, you can conquer every game with a sound that provides position accuracy and abstracts. Your everyday gaming is now double the fun with its RGB LED lights. Its lightweight and ergonomic build ensure you have a comfortable bender with your friends. Don’t look further, level up with Immortal 1000D headphones.",
+    price: 50,
+    quantity: 1
+  },
+  {
+    products: [pictures[20], pictures[21], pictures[22], pictures[23]],
+    name: 'BOAT BassHeads 900',
+    details: "The boAt BassHeads 900 wired headphones come with an amalgamation of style with performance. Now get ready to enjoy your favourite tunes with Super Extra Bass and crystal-clear sound performance with Bass Heads 900 wired headphones. Experience powerful, dynamic sound with punchy bass and clear, natural vocals with the responsive 40mm Neodymium drivers, the drivers effectively reproduce sound equally across the whole audio band. The ergonomic design creates passive attenuation of ambient noise while providing maximum comfort for hours of listening pleasure in a snuggly fit without fatigue. The sleek, lightweight, and compact design makes portability extremely convenient. The foldable and compact Feather Light Headphone weighs even less than 150 g, moreover, the ear cups can be folded for easy, convenient storage.",
+    price: 49,
+    quantity: 1
+  },
+  {
+    products: [pictures[24], pictures[25], pictures[26], pictures[27]],
+    name: 'BOAT Airdopes 281 Pro',
+    details: "Life seems better if the right music is heard the right way. Plug into a blissful listening experience with boAt Airdopes 281 Pro - a truly wireless earbuds powered by boAt Signature Sound. Equipped with ENx™ Technology and four mics, it makes your everyday calling experience hit the roof. Win matches while playing multiplayer games with your friends through its optimum noise cancellation and crystal-clear audio input. So, where are you? Because your perfect audio match is right here - boAt Airdopes 281.",
+    price: 69,
+    quantity: 1
+  },
+  {
+    products: [pictures[28], pictures[29], pictures[30], pictures[31]],
+    name: 'boAt Rockerz 451',
+    details: "Set your mind ablaze with boAt Rockerz 450 — our slick headphones that offer immersive sound quality and add luxury to your sound. Propelled by crystal clear 40mm dynamic drivers, slip into an alternate HD immersive audio reality. The soft cornered matte black finish allows for a comfortable fit, propagated by plush foam in adaptive and adjustable design. Choose your mode, go wireless with Bluetooth V4.2 or connect an aux wire that doesn’t cause any drain on the 300mAh rechargeable lithium battery.",
+    price: 49,
+    quantity: 1
+  }
+]
+
+export default data
