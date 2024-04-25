5139b8ff6c00c2c01b32f24a902e7c9241ff08e1
edmondhabimana <edmond.habimana@yahoo.com>
2024-04-24 20:35:54 -0700

    implement redux cartSlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import Provider from 'react-redux'
 import { ProductsProvider } from './context/ProductsContext.jsx'
 import { CartProvider } from './context/cartContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
+import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <ProductsProvider>
-        <App />
-      </ProductsProvider>
+      {/* <Provider store={store}> */}
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      {/* </Provider> */}
     </CartProvider>
   </React.StrictMode>,
 )
